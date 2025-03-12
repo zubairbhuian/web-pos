ecd056be6ecefa4986b0d8f70d6c1c40e2c9b88e
“Zubair <74538410+zubairbhuian@users.noreply.github.com>
2025-03-12 11:22:13 +0600

    socket issue fixed

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2603510592"
+    serviceWorkerVersion: "4074201169"
   }
 });
